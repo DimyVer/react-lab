e95e3334a97329e465772cb2f066d9567f62cadc
DimyVer <Dimych.veretennikov@mail.ru>
2025-05-01 00:36:33 +0500

    1_7_2 feat: вывод вложенных списков в компоненте RecipeList

--- src/1/1_7_2/App.tsx
+++ src/1/1_7_2/App.tsx
@@ -1,14 +1,21 @@
-// 1_7_2 Nested lists in one component 
-/*
-  Создайте список рецептов из этого массива! Для каждого рецепта в массиве выведите его название в виде <h2> и список ингредиентов в виде <ul>. Это потребует вложения двух различных вызовов map.
-*/
+// 1_7_2 feat: вывод вложенных списков в компоненте RecipeList
 
 import { recipes } from './data';
 
 export default function RecipeList() {
-    return (
-        <div>
-            <h1>Recipes</h1>
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
