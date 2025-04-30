889b0f5803864a2e2ff20e6e5fe254ecdbe61c9d
DimyVer <Dimych.veretennikov@mail.ru>
2025-04-30 21:10:25 +0500

    1_1_3 fix: имя компонента Profile приведено к верхнему регистру

--- src/1/1_1_3/App.tsx
+++ src/1/1_1_3/App.tsx
@@ -1,26 +1,18 @@
-// 1_1_3 Spot the mistake
-/* 
-  Исправьте ошибку в компоненте Profile.
-  Вспомните, как react отличает компоненты от обычных HTML-тегов.
-*/
+// 1_1_3 fix: имя компонента Profile приведено к верхнему регистру
 
-function profile() {
-    return (
-      <img
-        src="AlanHart.jpg"
-        alt="Alan L. Hart"
-      />
-    );
-  }
-  
-  export default function Gallery() {
-    return (
-      <section>
-        <h1>Amazing scientists</h1>
-        <profile />
-        <profile />
-        <profile />
-      </section>
-    );
-  }
-  
+function Profile() {
+  return (
+    <img src="AlanHart.jpg" alt="Alan L. Hart" />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
