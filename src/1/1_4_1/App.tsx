311e1fdd7d9a1d5e4bdf9e9349cf637d00c20d9c
DimyVer <Dimych.veretennikov@mail.ru>
2025-04-30 21:49:00 +0500

    1_4_1 fix: использовано свойство person.name вместо объекта person в компоненте TodoList

--- src/1/1_4_1/App.tsx
+++ src/1/1_4_1/App.tsx
@@ -1,31 +1,27 @@
-// 1_4_1 Fix the mistake
-/* 
-  Исправьте ошибку в коде.
-*/
+// 1_4_1 fix: использовано свойство person.name вместо объекта person в компоненте TodoList
 
 const person = {
-    name: 'Gregorio Y. Zara',
-    theme: {
-      backgroundColor: 'black',
-      color: 'pink'
-    }
-  };
-  
-  export default function TodoList() {
-    return (
-      <div style={person.theme}>
-        <h1>{person}'s Todos</h1>
-        <img
-          className="avatar"
-          src="GregorioYZara.jpg"
-          alt="Gregorio Y. Zara"
-        />
-        <ul>
-          <li>Improve the videophone</li>
-          <li>Prepare aeronautics lectures</li>
-          <li>Work on the alcohol-fuelled engine</li>
-        </ul>
-      </div>
-    );
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
   }
-  
+};
+
+export default function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="GregorioYZara.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
